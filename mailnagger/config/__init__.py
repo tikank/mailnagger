3145a246bb420df01d101866df7b3581c680b4f6
Timo Kankare <timo.kankare@iki.fi>
2024-11-22 17:05:21 +0200

    Fixed flake8 errors from main files.

--- mailnagger/config/__init__.py
+++ mailnagger/config/__init__.py
@@ -27,12 +27,9 @@ import subprocess
 import logging
 from gi.repository import Gtk
 
-from Mailnag.common.utils import fix_cwd, init_logging
+from Mailnag.common.utils import init_logging
 
-# fix_cwd()
-
-from Mailnag.common.i18n import _
-from Mailnag.common.utils import set_procname, shutdown_existing_instance, get_data_file, get_data_paths
+from Mailnag.common.utils import set_procname, shutdown_existing_instance, get_data_paths
 from Mailnag.common.dist_cfg import BIN_DIR
 from Mailnag.configuration.configwindow import ConfigWindow
 
@@ -40,52 +37,50 @@ LOG_LEVEL = logging.DEBUG
 
 
 class App(Gtk.Application):
-	def __init__(self):
-		Gtk.Application.__init__(self, application_id = 'com.github.tikank.mailnagger')
-		self.win = None
+    def __init__(self):
+        Gtk.Application.__init__(self, application_id = 'com.github.tikank.mailnagger')
+        self.win = None
 
 
-	def do_startup(self):
-		Gtk.Application.do_startup(self)
-		
-		# Add icons in alternative data paths (e.g. ./data/icons) 
-		# to the icon search path in case Mailnag is launched 
-		# from a local directory (without installing).
-		icon_theme = Gtk.IconTheme.get_default()
-		for path in get_data_paths():
-			icon_theme.append_search_path(os.path.join(path, "icons"))
-	
-	
-	def do_activate(self):
-		Gtk.Application.do_activate(self)
-		
-		if not self.win:
-			self.win = ConfigWindow(self)
-		self.win.get_gtk_window().present()
-	
-	
-	def do_shutdown(self):
-		Gtk.Application.do_shutdown(self)
-		
-		if self.win.get_daemon_enabled():
-			try:
-				# the launched daemon shuts down 
-				# an already running daemon
-				print("Launching Mailnagger daemon.")
-				subprocess.Popen(os.path.join(BIN_DIR, "mailnagger"))
-			except Exception as e:
-				print(f"ERROR: Failed to launch Mailnagger daemon: {str(e)}")
-		else:
-			# shutdown running Mailnagger daemon
-			shutdown_existing_instance(wait_for_completion = False)
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+        # Add icons in alternative data paths (e.g. ./data/icons) 
+        # to the icon search path in case Mailnag is launched 
+        # from a local directory (without installing).
+        icon_theme = Gtk.IconTheme.get_default()
+        for path in get_data_paths():
+            icon_theme.append_search_path(os.path.join(path, "icons"))
+
+    def do_activate(self):
+        Gtk.Application.do_activate(self)
+
+        if not self.win:
+            self.win = ConfigWindow(self)
+        self.win.get_gtk_window().present()
+
+
+    def do_shutdown(self):
+        Gtk.Application.do_shutdown(self)
+
+        if self.win.get_daemon_enabled():
+            try:
+                # the launched daemon shuts down 
+                # an already running daemon
+                print("Launching Mailnagger daemon.")
+                subprocess.Popen(os.path.join(BIN_DIR, "mailnagger"))
+            except Exception as e:
+                print(f"ERROR: Failed to launch Mailnagger daemon: {str(e)}")
+        else:
+            # shutdown running Mailnagger daemon
+            shutdown_existing_instance(wait_for_completion = False)
 
 
 def main():
-	set_procname("mailnagger-config")
-	init_logging(enable_stdout = True, enable_syslog = False, log_level = LOG_LEVEL)
-	app = App()
-	app.run(None)
-
+    set_procname("mailnagger-config")
+    init_logging(enable_stdout = True, enable_syslog = False, log_level = LOG_LEVEL)
+    app = App()
+    app.run(None)
 
 
 if __name__ == "__main__":  main()

e0f5e4ea80f5bfae4cedf7bcc35673029e94bb80
Timo Kankare <timo.kankare@iki.fi>
2024-12-10 16:20:21 +0200

    Added type information to plugindialog.

--- Mailnag/configuration/plugindialog.py
+++ Mailnag/configuration/plugindialog.py
@@ -1,3 +1,4 @@
+# Copyright 2024 Timo Kankare <timo.kankare@iki.fi>
 # Copyright 2013 - 2019 Patrick Ulbrich <zulu99@gmx.net>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -21,15 +22,24 @@ gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
 from Mailnag.common.i18n import _
+from Mailnag.common.plugins import Plugin
+
+from typing import Any
 
 
 class PluginDialog:
-	def __init__(self, parent, plugin):
+	def __init__(self, parent: Gtk.Window, plugin: Plugin):
 		self._plugin = plugin
 		
-		self._window = Gtk.Dialog(title = _('Plugin Configuration'), parent = parent, use_header_bar = True,
-			buttons = (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK))
-		
+		self._window = Gtk.Dialog(
+			title = _('Plugin Configuration'),
+			parent = parent,
+			use_header_bar = True
+		)
+		self._window.add_buttons(
+			Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+			Gtk.STOCK_OK, Gtk.ResponseType.OK
+		)
 		self._window.set_default_response(Gtk.ResponseType.OK)
 		self._window.set_default_size(480, 0)
 		
@@ -38,10 +48,10 @@ class PluginDialog:
 		self._box.set_spacing(12)
 
 		
-	def run(self):
+	def run(self) -> Any:
 		widget = self._plugin.get_config_ui()
 		
-		if widget != None:
+		if widget is not None:
 			self._box.pack_start(widget, True, True, 0)
 			widget.show_all()
 			self._plugin.load_ui_from_config(widget)
@@ -49,7 +59,7 @@ class PluginDialog:
 		res = self._window.run()
 		
 		if res == Gtk.ResponseType.OK:
-			if widget != None:
+			if widget is not None:
 				self._plugin.save_ui_to_config(widget)
 		
 		self._window.destroy()
